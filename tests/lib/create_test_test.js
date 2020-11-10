4207cc107eb1af1af26df07136a13207707fd766
Alexey D <ord.alwo@gmail.com>
2020-11-10 14:51:44 +0300

    Rename test to create_test

--- tests/lib/create_test_test.js
+++ tests/lib/create_test_test.js
@@ -1,5 +1,5 @@
 import { strict as assert } from 'assert';
-import { createTest } from "test";
+import createTest from "create_test";
 
 async function testHasExpectations() {
   let name = 'runTest() passes expectations object to {testBody}';
