3d36f27af9a40da6e95827ee41a9ebc9ca79a63f
Alexey D <ord.alwo@gmail.com>
2020-11-11 10:13:20 +0300

    utils/promise isPromise()

--- test/lib/utils/promise_test.js
+++ test/lib/utils/promise_test.js
@@ -1,5 +1,5 @@
 import jutest from 'jutest';
-import { resolveAsyncFunctionsInOrder } from "utils/promise";
+import { resolveAsyncFunctionsInOrder, isPromise } from "utils/promise";
 
 jutest('utils/promise', s => {
   s.describe('resolveAsyncFunctionsInOrder()', s => {
@@ -21,6 +21,19 @@ jutest('utils/promise', s => {
       t.equal(results.length, 2);
       t.equal(results[0], 'slow function');
       t.equal(results[1], 'fast function');
+    });
+  });
+
+  s.describe("isPromise()", s => {
+    s.test("returns true if object is a promise", t => {
+      let promise = Promise.resolve();
+      t.equal(isPromise(promise), true);
+    });
+
+    s.test("returns false if object is not a promise", t => {
+      t.equal(isPromise(null), false);
+      t.equal(isPromise(undefined), false);
+      t.equal(isPromise({}), false);
     });
   });
 });
