f91f02dbedac0a6ac7a183feeb447fee677ef54f
Alexey D <ord.alwo@gmail.com>
2020-11-12 14:57:02 +0300

    Rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,17 @@
 import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
+import resolve from '@rollup/plugin-node-resolve';
 
 export default [
   {
     input: 'lib/jutest.js',
-    output: { file: pkg.main, format: 'cjs', indent: false },
+    output: { file: pkg.main, format: 'cjs', indent: false, exports: 'default' },
     external: [
       ...Object.keys(pkg.dependencies || {}),
-      ...Object.keys(pkg.peerDependencies || {})
+      ...Object.keys(pkg.peerDependencies || {}),
+      'path',
+      'util'
     ],
-    plugins: [babel()]
+    plugins: [babel(), resolve({preferBuiltins: true})]
   }
 ];
