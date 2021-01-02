a86a35ed91d574f3a1a8290ba6ea14bc85f856b5
Alexey D <ord.alwo@gmail.com>
2021-01-02 19:55:00 +0300

    Assertion refactoring

--- test/lib/assertions/assertion_test.js
+++ test/lib/assertions/assertion_test.js
@@ -2,40 +2,60 @@ import jutest from 'jutest';
 import Assertion from "assertions/assertion";
 
 jutest('Assertion', s => {
-  s.describe('constructor', s => {
-    s.test('creates assertion with given properties', t => {
-      let assertion = new Assertion({
-        expected: 1,
-        actual: 2,
-        passed: false,
-        operator: 'foobar'
-      });
+  s.describe("::create", s => {
+    s.test("creates passed assertion", t => {
+      let assertion = Assertion.create({expected: 1, actual: 1, passed: true, operator: 'equal'});
 
+      t.equal(assertion.passed, true);
+      t.equal(assertion.failureMessage, null);
+      t.equal(assertion.actual, 1);
+      t.equal(assertion.expected, 1);
+      t.equal(assertion.operator, 'equal');
+    });
+
+    s.test("creates failed assertion", t => {
+      let assertion = Assertion.create({expected: 1, actual: 2, passed: false, operator: 'equal'});
+
+      t.equal(assertion.passed, false);
+      t.match(assertion.failureMessage, /equal/);
       t.equal(assertion.expected, 1);
       t.equal(assertion.actual, 2);
-      t.equal(assertion.passed, false);
-      t.equal(assertion.operator, 'foobar');
+      t.equal(assertion.operator, 'equal');
+    });
+
+    s.test("accepts {buildFailureMessage} option", t => {
+      let buildFailureMessage = ({actual, expected, operator}) => `${actual} not ${operator} ${expected}`;
+      let assertion = Assertion.create({passed: false, operator: 'equal', expected: 2, actual: 1, buildFailureMessage});
+
+      t.equal(assertion.failureMessage, "1 not equal 2");
     });
   });
 
-  s.describe('#failureDetails', s => {
-    s.test('inspects assertion', t => {
-      let assertion = new Assertion({
-        expected: 1,
-        actual: 2,
-        passed: false,
-        operator: 'foobar'
-      });
+  s.describe("::negate", s => {
+    s.test("negates passed assertion", t => {
+      let assertion = Assertion.create({expected: 1, actual: 1, passed: true, operator: 'equal'});
+      let negated = Assertion.negate(assertion, { operator: 'notEqual' });
 
-      t.assert(!!assertion.failureDetails.match('foobar'));
+      t.equal(negated.operator, 'notEqual');
+      t.equal(negated.passed, false);
+      t.match(negated.failureMessage, /notEqual/);
     });
 
-    s.test('uses provided getFailureDetails function', t => {
-      let assertion = new Assertion({
-        getFailureDetails: () => 'custom failure details'
-      });
+    s.test("negates failed assertion", t => {
+      let assertion = Assertion.create({expected: 1, actual: 2, passed: false, operator: 'equal'});
+      let negated = Assertion.negate(assertion, { operator: 'notEqual' });
 
-      t.equal(assertion.failureDetails, 'custom failure details');
+      t.equal(negated.operator, 'notEqual');
+      t.equal(negated.passed, true);
+      t.equal(negated.failureMessage, null);
+    });
+
+    s.test("accepts {buildFailureMessage} option", t => {
+      let buildFailureMessage = ({actual, expected, operator}) => `${actual} should be ${operator} ${expected}`;
+      let assertion = Assertion.create({expected: 1, actual: 1, passed: true, operator: 'equal'});
+      let negated = Assertion.negate(assertion, { operator: 'notEqual', buildFailureMessage });
+
+      t.equal(negated.failureMessage, "1 should be notEqual 1");
     });
   });
 });
