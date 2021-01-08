12e064e9287191e393933bd6eaa82b8b09eb51d5
Alexey D <ord.alwo@gmail.com>
2021-01-08 10:21:34 +0300

    TestResult

---
+++ tests/lib/test/test_result_test.js
@@ -0,0 +1,24 @@
+import jutest from "jutest";
+import TestResult from "test/test_result";
+
+jutest("TestResult", s => {
+  s.describe("::passed", s => {
+    s.test("returns passed test result", t => {
+      let result = TestResult.passed({ testName: 'foobar' });
+
+      t.equal(result.passed, true);
+      t.equal(result.error, null);
+      t.equal(result.testName, 'foobar');
+    });
+  });
+
+  s.describe("::errored", s => {
+    s.test("returns errored test result", t => {
+      let result = TestResult.errored({ testName: 'foobar', error: "baz" });
+  
+      t.equal(result.passed, false);
+      t.equal(result.error, "baz");
+      t.equal(result.testName, 'foobar');
+    });
+  });
+});
