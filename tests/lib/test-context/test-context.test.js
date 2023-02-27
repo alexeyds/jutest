e063bfdc0ab779b6c09c0328309889df062017ac
Alexey <ord.alwo@gmail.com>
2023-02-27 21:18:22 +0400

    Refactore TestContext and add missing callbacks

--- tests/lib/test-context/test-context.test.js
+++ tests/lib/test-context/test-context.test.js
@@ -1,93 +1,117 @@
 import jutest from "jutest";
-import { createTestContext, addToContext, runSetups } from "test-context";
+import { TestContext } from "test-context";
 
-jutest("test-context", s => {
-  s.describe("createTestContext", s => {
-    s.test("returns basic context", t => {
-      let context = createTestContext();
+jutest("TestContext", s => {
+  s.setup(() => {
+    return { context: new TestContext() };
+  });
 
-      t.same(context.names, []);
-      t.same(context.setups, []);
-      t.same(context.teardowns, []);
-      t.same(context.beforeTestAssertions, []);
-      t.same(context.afterTestAssertions, []);
+  s.describe('addName', s => {
+    s.test('adds name to context', (t, { context }) => {
+      context.addName('testing');
+      t.same(context.names, ['testing']);
     });
   });
 
-  s.describe('addToContext', s => {
-    s.test("adds items to specified array", t => {
-      let context = createTestContext();
-      let newContext = addToContext(context, 'names', 'test');
+  s.describe("copy", s => {
+    s.test("copies current context", (t, { context }) => {
+      context.addName('test');
+      let newContext = context.copy();
 
       t.same(newContext.names, ['test']);
     });
 
-    s.test("creates different context object", t => {
-      let context = createTestContext();
-      let newContext = addToContext(context, 'names', 'test');
+    s.test("keeps context states separate", (t, { context }) => {
+      context.addName('foo');
+      let newContext = context.copy();
+      newContext.addName('bar');
 
-      t.notEqual(context, newContext);
+      t.same(context.names, ['foo']);
     });
   });
 
-  s.describe('runSetups', s => {
-    s.test("does nothing if there are no setups", async () => {
-      let context = createTestContext();
-      await runSetups(context);
+  s.describe("runSetups", s => {
+    s.test("runs added setups", async (t, { context }) => {
+      let result;
+      context.addSetup(() => result = 1);
+      await context.runSetups();
+
+      t.equal(result, 1);
     });
 
-    s.test('runs added setups', async t => {
-      let a;
-
-      let context = contextWithSetups(
-        () => a = 1
-      );
-
-      await runSetups(context);
-
-      t.equal(a, 1);
-    });
-
-    s.test('merges all setups in order and returns the result', async t => {
-      let context = contextWithSetups(
-        () => ({ a: 1, b: 2 }),
-        () => ({ b: 3, c: 4 })
-      );
-      let result = await runSetups(context);
+    s.test('merges all setup results together', async (t, { context }) => {
+      context.addSetup(() => ({ a: 1, b: 2 }));
+      context.addSetup(() => ({ b: 3, c: 4 }));
+      let result = await context.runSetups();
 
       t.same(result, { a: 1, b: 3, c: 4 });
     });
 
-    s.test('works if setups return non-mergeable values', async t => {
-      let context = contextWithSetups(
-        () => 'foo',
-        () => 'bar'
-      );
-      let result = await runSetups(context);
+    s.test('passes previous setup result to the next', async (t, { context }) => {
+      let multiplier = ({ result }) => {
+        return { result: result*2 };
+      };
+
+      context.addSetup(() => ({ result: 5 }));
+      context.addSetup(multiplier);
+      context.addSetup(multiplier);
+      let { result } = await context.runSetups();
+
+      t.equal(result, 20);
+    });
+
+    s.test("works even if setup returns non-mergeable value", async (t, { context }) => {
+      context.addSetup(() => 'asd');
+      let result = await context.runSetups();
 
       t.same(result, {});
     });
+  });
 
-    s.test("passes assigns to next setup", async t => {
-      let passedAssigns;
+  s.describe("runTeardowns", s => {
+    s.test("runs added teardowns", async (t, { context }) => {
+      let result;
+      context.addTeardown(() => result = 1);
+      await context.runTeardowns();
 
-      let context = contextWithSetups(
-        () => ({ a: 1 }),
-        (assigns) => passedAssigns = assigns
-      );
-      await runSetups(context);
+      t.equal(result, 1);
+    });
 
-      t.same(passedAssigns, {a: 1});
+    s.test("runs teardowns in order", async (t, { context }) => {
+      let results = [];
+      context.addTeardown(() => Promise.resolve().then(() => results.push(1)));
+      context.addTeardown(() => results.push(2));
+      await context.runTeardowns();
+
+      t.same(results, [1, 2]);
+    });
+
+    s.test("passes provided arguments to teardowns", async (t, { context }) => {
+      let result;
+      context.addTeardown((a, b) => result = a - b);
+      await context.runTeardowns(10, 5);
+
+      t.same(result, 5);
+    });
+  });
+
+  s.describe('runAfterTestAssertions', s => {
+    s.test("runs added assertions", async (t, { context }) => {
+      let result;
+      context.addAfterTestAssertion((a, b) => result = a - b);
+      await context.runAfterTestAssertions(10, 5);
+
+      t.same(result, 5);
+    });
+  });
+
+  s.describe('runBeforeTestAssertions', s => {
+    s.test("runs added assertions", async (t, { context }) => {
+      let result;
+      context.addBeforeTestAssertion((a, b) => result = a - b);
+      await context.runBeforeTestAssertions(10, 5);
+
+      t.same(result, 5);
     });
   });
 });
-
-function contextWithSetups(...setups) {
-  let context = createTestContext();
-
-  setups.forEach(setup => {
-    context = addToContext(context, 'setups', setup);
-  });
-
-  return context;
-}
