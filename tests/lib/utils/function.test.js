fd35c7c3888351ea5ceba18e4b46b9bd49441a11
Alexey <ord.alwo@gmail.com>
2023-03-30 20:38:35 +0400

    createAssertions

---
+++ tests/lib/utils/function.test.js
@@ -0,0 +1,13 @@
+import jutest from "jutest";
+import { nameFunction } from "utils/function";
+
+jutest("utils/function", s => {
+  s.describe("nameFunction()", s => {
+    s.test("assigns name to a function", t => {
+      let func = () => {};
+      nameFunction(func, 'foobar');
+
+      t.equal(func.name, 'foobar');
+    });
+  });
+});
