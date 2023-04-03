5ace5368e3e27ca5fa79cb20c911f71b7ca470f7
Alexey <ord.alwo@gmail.com>
2023-04-03 20:02:50 +0400

    matcher-result

---
+++ tests/lib/assertions/utils/matcher-result.test.js
@@ -0,0 +1,22 @@
+import jutest from "jutest";
+import { success, failure } from "assertions/utils/matcher-result";
+
+jutest("matcher-result", s => {
+  s.describe("success()", s => {
+    s.test("returns successfull result", t => {
+      let result = success();
+
+      t.equal(result.passed, true);
+      t.refute(result.failureMessage);
+    });
+  });
+
+  s.describe("failure()", s => {
+    s.test("returns failed result", t => {
+      let result = failure('test');
+
+      t.equal(result.passed, false);
+      t.equal(result.failureMessage, 'test');
+    });
+  });
+});
