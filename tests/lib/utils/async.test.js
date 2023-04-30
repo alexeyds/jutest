71f8321c2dd67e91b67de089de28d83806ff5d86
Alexey <ord.alwo@gmail.com>
2023-04-30 23:33:56 +0400

    Remove reduceAsync

--- tests/lib/utils/async.test.js
+++ tests/lib/utils/async.test.js
@@ -1,5 +1,5 @@
 import jutest from "jutest";
-import { reduceAsync, createOrderedResolver } from "utils/async";
+import { createOrderedResolver } from "utils/async";
 
 jutest("utils/async", s => {
   s.describe("createOrderedResolver", s => {
@@ -25,25 +25,6 @@ jutest("utils/async", s => {
       await resolver('test');
 
       t.equal(result, 'test');
-    });
-  });
-
-  s.describe("reduceAsync", s => {
-    s.test("returns initial value if array is empty", async t => {
-      let initalValue = {};
-      let result = await reduceAsync([], initalValue, () => {});
-
-      t.equal(result, initalValue);
-    });
-
-    s.test("passes every value and acc through reducer function", async t => {
-      let result = await reduceAsync([1, 2, 3], 0, (acc, val) => acc+val);
-      t.equal(result, 6);
-    });
-
-    s.test("supports async reducers", async t => {
-      let result = await reduceAsync([1, 2, 3], 0, async (acc, val) => acc+val);
-      t.equal(result, 6);
     });
   });
 });
