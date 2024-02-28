4ca8cc81e1555f218d1372efca5ce0b014f52243
Alexey Desheulin <ord.alwo@gmail.com>
2024-02-29 00:04:12 +0400

    SourceLocator#setSourceFile

--- tests/lib/utils/source-locator.test.js
+++ tests/lib/utils/source-locator.test.js
@@ -3,6 +3,7 @@ import { SourceLocator } from "utils/source-locator";
 
 jutest("utils/source-locator", s => {
   let currentFileRegexp = /source-locator\.test/;
+  let ownFileName = 'source-locator.test.js';
 
   s.describe("#locations", s => {
     s.test("returns parsed source locations", t => {
@@ -10,7 +11,7 @@ jutest("utils/source-locator", s => {
       let location = locator.locations[1];
 
       t.match(location.stackFrame, currentFileRegexp);
-      t.equal(location.lineNumber, 9);
+      t.equal(location.lineNumber, 10);
     });
 
     s.test("caches the definitions", t => {
@@ -33,7 +34,7 @@ jutest("utils/source-locator", s => {
       let locator = new SourceLocator();
       let lineNumber = locator.guessLineNumberInFile('source-locator.test.js');
 
-      t.equal(lineNumber, 33);
+      t.equal(lineNumber, 34);
     });
 
     s.test("returns latest source line from multiple definitions", t => {
@@ -45,7 +46,7 @@ jutest("utils/source-locator", s => {
 
       let lineNumber = locator.guessLineNumberInFile('source-locator.test.js');
 
-      t.equal(lineNumber, 40);
+      t.equal(lineNumber, 41);
     });
 
     s.test("returns undefined when location is not found", t => {
@@ -53,6 +54,22 @@ jutest("utils/source-locator", s => {
       let lineNumber = locator.guessLineNumberInFile('foobar.test.js');
 
       t.equal(lineNumber, undefined);
+    });
+  });
+
+  s.describe("#setSourceFile", s => {
+    s.test("sets file to the provided one", t => {
+      let locator = new SourceLocator();
+      locator.setSourceFile('foobar.test.js');
+
+      t.equal(locator.filePath, 'foobar.test.js');
+    });
+
+    s.test("sets found location from the given file", t => {
+      let locator = new SourceLocator();
+      locator.setSourceFile(ownFileName);
+
+      t.equal(locator.lineNumber, 69);
     });
   });
 });
