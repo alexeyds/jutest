e6fa215b1b5606e60833242d67e91e088ddf3a63
Alexey Desheulin <ord.alwo@gmail.com>
2024-03-15 04:37:15 +0400

    test-runner/load-specs

---
+++ tests/lib/test-runner/load-specs/filter-specs.test.js
@@ -0,0 +1,54 @@
+import { jutest } from "jutest";
+import { Jutest } from "core";
+import { TestRunnerContext } from "test-runner/context";
+import { filterSpecs } from "test-runner/load-specs/filter-specs";
+
+let ownFileName = 'filter-specs.test.js';
+
+jutest("findRunnableSpecs", s => {
+  s.setup(() => {
+    let jutestInstance = new Jutest();
+    let context = TestRunnerContext.forSingleLocation('foo.test.js');
+
+    return { jutestInstance, context };
+  });
+
+  s.test("returns all specs by default", async (t, { jutestInstance, context }) => {
+    jutestInstance.test('test1');
+    jutestInstance.test('test2');
+
+    let specs = await filterSpecs(jutestInstance, context);
+
+    t.equal(specs.length, 2);
+    t.equal(specs[0].name, 'test1');
+    t.equal(specs[1].name, 'test2');
+  });
+
+  s.test("returns tests defined on the specified line", async (t, { jutestInstance }) => {
+    let context = TestRunnerContext.forSingleLocation(ownFileName, 32);
+
+    jutestInstance.specsContainer.sourceFilePath = ownFileName;
+    jutestInstance.test('test1');
+    jutestInstance.test('test2');
+
+    let specs = await filterSpecs(jutestInstance, context);
+
+    t.equal(specs.length, 1);
+    t.equal(specs[0].name, 'test2');
+  });
+
+  s.test("works with suites", async (t, { jutestInstance }) => {
+    let context = TestRunnerContext.forSingleLocation(ownFileName, 46);
+
+    jutestInstance.specsContainer.sourceFilePath = ownFileName;
+    jutestInstance.describe('test', s => {
+      s.test('test1');
+      s.test('test2');
+    });
+
+    let specs = await filterSpecs(jutestInstance, context);
+
+    t.equal(specs.length, 1);
+    t.equal(specs[0].name, 'test test2');
+  });
+});
---
+++ tests/lib/test-runner/load-specs/load-files.test.js
@@ -0,0 +1,52 @@
+import { jutest } from "jutest";
+import { spy } from "sinon";
+import { Jutest } from "core";
+import { TestRunnerContext } from "test-runner/context";
+import { loadFiles } from "test-runner/load-specs/load-files";
+
+jutest("loadFiles", s => {
+  s.setup(() => {
+    let requireSpy = spy();
+    let jutestInstance = new Jutest();
+    let context = TestRunnerContext.forSingleLocation('foo.test.js');
+
+    return { requireSpy, context, jutestInstance };
+  });
+
+  s.test("calls the require func with specified files", async (t, { requireSpy, jutestInstance }) => {
+    let file = 'foobar.test.js';
+    let context = TestRunnerContext.forSingleLocation(file);
+    await loadFiles(jutestInstance, context, requireSpy);
+
+    t.assert(requireSpy.called);
+    t.equal(requireSpy.firstCall.args[0], file);
+  });
+
+  s.test("sets source path for loaded specs", async (t, { context, jutestInstance }) => {
+    let requireFunc = () => {
+      jutestInstance.test('foo', () => {});
+    };
+
+    await loadFiles(jutestInstance, context, requireFunc);
+    let [test] = jutestInstance.specs;
+
+    t.assert(test.sourceLocator.sourceFilePath);
+  });
+
+  s.test("composes loaded specs", async (t, { context, jutestInstance }) => {
+    let requireFunc = () => {
+      jutestInstance.describe('foo', () => {});
+    };
+
+    await loadFiles(jutestInstance, context, requireFunc);
+    let [suite] = jutestInstance.specs;
+
+    t.assert(suite.isComposed);
+  });
+
+  s.test("locks specs container after files are loaded", async (t, { context, jutestInstance, requireSpy }) => {
+    await loadFiles(jutestInstance, context, requireSpy);
+
+    t.throws(() => jutestInstance.test('asd'), /lock/);
+  });
+});
