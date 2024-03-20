3f6e88ad72a56a24b97d4f021abce00d680ee0a2
Alexey Desheulin <ord.alwo@gmail.com>
2024-03-20 04:04:01 +0400

    Move async job to async

--- tests/lib/utils/async.test.js
+++ tests/lib/utils/async.test.js
@@ -1,5 +1,5 @@
 import { jutest } from "jutest";
-import { createOrderedResolver } from "utils/async";
+import { createOrderedResolver, AsyncJob } from "utils/async";
 
 jutest("utils/async", s => {
   s.describe("createOrderedResolver", s => {
@@ -25,6 +25,39 @@ jutest("utils/async", s => {
       await resolver('test');
 
       t.equal(result, 'test');
+    });
+  });
+
+  s.describe("AsyncJob", s => {
+    s.test("has initial attributes", t => {
+      let job = new AsyncJob(() => 1);
+
+      t.equal(job.wasRun, false);
+      t.equal(job.result, undefined);
+    });
+
+    s.test("executes given function", async t => {
+      let job = new AsyncJob(() => 1);
+
+      let promise = job.run();
+      t.equal(job.wasRun, false);
+
+      let result = await promise;
+      t.equal(result, 1);
+      t.equal(job.wasRun, true);
+      t.equal(job.result, 1);
+    });
+
+    s.test("always returns same promise", async t => {
+      let job = new AsyncJob(() => Promise.resolve(Math.random()));
+
+      let promise1 = job.run();
+      let promise2 = job.run();
+      let [result1, result2] = await Promise.all([promise1, promise2]);
+
+      t.equal(result1, result2);
+      t.assert(job.result);
+      t.equal(job.result, result1);
     });
   });
 });
