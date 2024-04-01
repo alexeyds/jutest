870299c0ef75a3170c7c62de84dcc4a95949352d
Alexey Desheulin <ord.alwo@gmail.com>
2024-04-01 05:33:14 +0400

    SourceLocator -> TestSourceLocator

--- tests/lib/utils/test-source-locator.test.js
+++ tests/lib/utils/test-source-locator.test.js
@@ -1,12 +1,12 @@
 import { jutest } from "jutest";
-import { SourceLocator } from "utils/source-locator";
+import { TestSourceLocator } from "utils/test-source-locator";
 
-jutest("SourceLocator", s => {
-  let ownFileName = 'source-locator.test.js';
+jutest("TestSourceLocator", s => {
+  let ownFileName = 'test-source-locator.test.js';
 
   s.describe("constructor()", s => {
     s.test("sets default attributes", t => {
-      let locator = new SourceLocator();
+      let locator = new TestSourceLocator();
 
       t.assert(locator.sourceError);
       t.refute(locator.sourceFilePath);
@@ -14,37 +14,37 @@ jutest("SourceLocator", s => {
 
     s.test("accepts sourceError", t => {
       let sourceError = new Error();
-      let locator = new SourceLocator({ sourceError });
+      let locator = new TestSourceLocator({ sourceError });
 
       t.equal(locator.sourceError, sourceError);
     });
 
     s.test("accepts sourceFilePath", t => {
-      let locator = new SourceLocator({ sourceFilePath: ownFileName });
+      let locator = new TestSourceLocator({ sourceFilePath: ownFileName });
       t.equal(locator.sourceFilePath, ownFileName);
     });
   });
 
   s.describe("#lineNumbers", s => {
     s.test("returns empty array by default", t => {
-      let locator = new SourceLocator();
+      let locator = new TestSourceLocator();
       t.same(locator.lineNumbers, []);
     });
 
     s.test("returns line number in source file path", t => {
-      let locator = new SourceLocator({ sourceFilePath: ownFileName });
+      let locator = new TestSourceLocator({ sourceFilePath: ownFileName });
       t.same(locator.lineNumbers, [35]);
     });
 
     s.test("returns undefined if line not found in the file", t => {
-      let locator = new SourceLocator({ sourceFilePath: 'foo.test.js' });
+      let locator = new TestSourceLocator({ sourceFilePath: 'foo.test.js' });
       t.same(locator.lineNumbers, []);
     });
 
     s.test("correctly handles error with stack frames without files", t => {
       let error = new Error();
       error.stack = 'at <anonymous>';
-      let locator = new SourceLocator({ sourceError: error, sourceFilePath: 'foo.test.js' });
+      let locator = new TestSourceLocator({ sourceError: error, sourceFilePath: 'foo.test.js' });
       
       t.same(locator.lineNumbers, []);
     });
@@ -52,12 +52,12 @@ jutest("SourceLocator", s => {
 
   s.describe("#lineNumber", s => {
     s.test("returns undefined by default", t => {
-      let locator = new SourceLocator();
+      let locator = new TestSourceLocator();
       t.refute(locator.lineNumber);
     });
 
     s.test("returns line number in source file path", t => {
-      let locator = new SourceLocator({ sourceFilePath: ownFileName });
+      let locator = new TestSourceLocator({ sourceFilePath: ownFileName });
       t.assert(locator.lineNumber);
     });
   });
