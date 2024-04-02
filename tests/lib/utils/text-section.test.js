e79088fcbf1a2dd55fbda6ce9be708da509689d2
Alexey Desheulin <ord.alwo@gmail.com>
2024-04-02 07:40:57 +0400

    Fix multi-line assertion message

--- tests/lib/utils/text-section.test.js
+++ tests/lib/utils/text-section.test.js
@@ -1,32 +1,32 @@
 import { jutest } from "jutest";
-import { buildSection } from "reporters/shared";
+import { buildTextSection } from "utils/text-section";
 
-jutest("buildSection", s => {
+jutest("buildTextSection", s => {
   s.test("returns empty string if builder is empty", t => {
-    let section = buildSection(() => {});
+    let section = buildTextSection(() => {});
     t.equal(section, '');
   });
 
   s.describe("{ padding } option", s => {
     s.test("adds spaces to all added lines", t => {
-      let string = buildSection(s => s.addLine('testing'), { padding: 2 });
+      let string = buildTextSection(s => s.addLine('testing'), { padding: 2 });
       t.equal(string, '  testing\n');
     });
 
     s.test("does not add spaces to empty lines", t => {
-      let string = buildSection(s => s.addLine(), { padding: 2 });
+      let string = buildTextSection(s => s.addLine(), { padding: 2 });
       t.equal(string, '\n');
     });
   });
 
   s.describe("builder#addLine", s => {
     s.test("adds line to the result", t => {
-      let string = buildSection(s => s.addLine('testing'));
+      let string = buildTextSection(s => s.addLine('testing'));
       t.equal(string, 'testing\n');
     });
 
     s.test("allows adding multiple lines", t => {
-      let string = buildSection(s => {
+      let string = buildTextSection(s => {
         s.addLine('testing');
         s.addLine('123');
       });
@@ -35,19 +35,19 @@ jutest("buildSection", s => {
     });
 
     s.test("adds empty line if no content is provided", t => {
-      let string = buildSection(s => s.addLine());
+      let string = buildTextSection(s => s.addLine());
       t.equal(string, '\n');
     });
 
     s.test("allows adding multi-line text", t => {
-      let string = buildSection(s => s.addLine('testing'));
+      let string = buildTextSection(s => s.addLine('testing'));
       t.equal(string, 'testing\n');
     });
   });
 
   s.describe("builder#addSection", s => {
     s.test("creates nested section", t => {
-      let string = buildSection(s => {
+      let string = buildTextSection(s => {
         s.addSection(s => {
           s.addLine('testing');
         });
@@ -57,7 +57,7 @@ jutest("buildSection", s => {
     });
 
     s.test("combines paddings correctly", t => {
-      let string = buildSection(s => {
+      let string = buildTextSection(s => {
         s.addSection(s => {
           s.addLine('testing');
         }, { padding: 2 });
