b6eecbf531d99d93b3c28ec1ca67ae54eae1a8d5
Alexey Desheulin <ord.alwo@gmail.com>
2024-06-25 08:24:06 +0400

    Jutest types

--- lib/types/jutest.d.ts
+++ lib/types/jutest.d.ts
@@ -1,36 +1,56 @@
 export class AssertionFailedError extends Error {};
-export const jutest: jutest;
+export const jutest: Jutest;
 
-type jutest =  describe & SuiteBuilderAPI & JutestConfigurationAPI;
-
-interface JutestConfigurationAPI {
-  configureNewInstance: (configure: configurationBuilder) => jutest;
+interface Jutest extends Describe, SuiteBuilderAPI {
+  configureNewInstance: ConfigureNewInstance;
 }
 
-type configurationBuilder = (context: TestContextAPI) => void;
+interface ConfigureNewInstance {
+  (jutestBuilder: (api: TestContextAPI) => void): Jutest;
+}
+
+interface Describe {
+  (name?: string, body?: SuiteBody): void;
+  (name: string, tags: Tags, body: SuiteBody): void;
+}
+interface Test {
+  (name?: string, body?: TestBody): void;
+  (name: string, tags: Tags, body: TestBody): void;
+}
+
+interface SuiteBody {
+  (suiteApi: SuiteAPI): void;
+}
+
+interface SuiteAPI extends TestContextAPI, SuiteBuilderAPI {};
 
 interface SuiteBuilderAPI {
-  describe: describe;
-  xdescribe: describe;
-  test: test;
-  xtest: test;
+  describe: Describe;
+  xdescribe: Describe;
+  test: Test;
+  xtest: Test;
 }
-
-type describe = (name?: string, body?: suiteBody) => void;
-type suiteBody = (suiteApi: SuiteAPI) => void;
-
-type SuiteAPI = TestContextAPI & SuiteBuilderAPI;
 
 interface TestContextAPI {
-  assertBeforeTest: testBody;
-  assertAfterTest: testBody;
-  setup: (assings: Assigns) => any;
-  teardown: (assings: Assigns) => void;
+  assertBeforeTest: TestBody;
+  assertAfterTest: TestBody;
+  setup: Setup;
+  teardown: Teardown;
   addName: (name: string) => void;
+  addTags: (tags: Tags) => void;
 }
 
-type test = (name?: string, body?: testBody) => void;
-type testBody = (t: Assertions, assigns: Assigns) => void;
+interface Setup {
+  (setupFunc: (assigns: Assigns, tags: Tags) => Assigns | void): void;
+}
+
+interface Teardown {
+  (teardownFunc: (assigns: Assigns, tags: Tags) =>  void): void;
+}
+
+interface TestBody {
+  (t: Assertions, assigns: Assigns): void;
+}
 
 interface Assertions {
   same: twoArgAssertion;
@@ -49,12 +69,16 @@ interface Assertions {
 }
 
 type twoArgAssertion = (actual: any, expected: any) => void;
-type oneArgAsssertion = (actual: any): void;
+type oneArgAsssertion = (actual: any) => void;
 type includeAssertion = (actual: { includes: (item: any) => any }, expected: any) => void;
 
 interface AsyncAssertions {
   rejects: (promise: any, matcher: any) => Promise;
-  passesEventually: (func: function, options?: { timeout: number, interval: number }) => Promise;
+  passesEventually: (func: function, options?: { timeout?: number, interval?: number }) => Promise;
+}
+
+interface Tags {
+  [key: string]: any;
 }
 
 interface Assigns {
